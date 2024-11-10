22e7beacf19ebb3cf0540e6d0c146bf6a090131a
karthik261201 <karthiknarayanan043@gmail.com>
2024-11-10 18:27:08 +0530

    added global loading

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,6 @@
-import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom"
+import { Outlet, redirect, useLoaderData, useNavigate, useNavigation } from "react-router-dom"
 import Wrapper from '../assets/wrappers/Dashboard';
-import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { BigSidebar, Loading, Navbar, SmallSidebar } from "../components";
 import { createContext, useContext, useState } from "react";
 import { checkDefaultTheme } from "../App";
 import customFetch from "../utils/customFetch";
@@ -10,7 +10,8 @@ const DashboardContext = createContext()
 const DashboardLayout = () => {
 
     const { user } = useLoaderData();
-    // const user = {name: "John"}
+    const navigation = useNavigation();
+    const isPageLoading = navigation.state === 'loading';
     const navigate = useNavigate()
     const [showSidebar, setShowSidebar] = useState(false)
     const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme())
@@ -41,7 +42,7 @@ const DashboardLayout = () => {
                 <div>
                     <Navbar />
                     <div className='dashboard-page'>
-                    <Outlet context={{user}}/>
+                        {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
                     </div>
                 </div>
                 </main>
